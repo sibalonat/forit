9ab7839c392bbb589e4a8fa51abfd369d561f36f
Marin Nikolli <sibalonat@gmail.com>
2022-09-03 23:04:44 +0200

    justit

--- resources/js/Pages/FormTemplate/JustIt.vue
+++ resources/js/Pages/FormTemplate/JustIt.vue
@@ -91,17 +91,14 @@ const components = {
 const element = ref(null)
 
 const containerResizedEvent = (e) => {
-    console.log(e);
     emit('contain', e)
 }
 
 const layoutUpdatedEvent = (e) => {
-    console.log(e);
     emit('layoutChanged', e)
 }
 
 const setStateItem = (e) => {
-    // console.log(e);
     if (e.stateitem === false) {
         e.stateitem = !e.stateitem
     } else if (e.stateitem === true) {
@@ -117,12 +114,6 @@ const hideReplace = (e) => {
 const rowHeight = computed(() => (window.innerWidth - 56) / 12)
 const fields = computed(() => fieldlist.value)
 
-// const component = computed({
-//     get: () => count.value + 1,
-//     set: (val) => {
-//         count.value = val - 1
-//     }
-// })
 
 const colNum = inject('colNum')
 const elements = inject('elements')
@@ -259,13 +250,5 @@ onMounted(() => {
     cursor: pointer;
 }
 
-/* style="background-image: url("data:image/svg+xml;utf8, <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                    <path fill-rule="evenodd"
-                        d="M2.25 4.125c0-1.036.84-1.875 1.875-1.875h5.25c1.036 0 1.875.84 1.875 1.875V17.25a4.5 4.5 0 11-9 0V4.125zm4.5 14.25a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z"
-                        clip-rule="evenodd" />
-                    <path
-                        d="M10.719 21.75h9.156c1.036 0 1.875-.84 1.875-1.875v-5.25c0-1.036-.84-1.875-1.875-1.875h-.14l-8.742 8.743c-.09.089-.18.175-.274.257zM12.738 17.625l6.474-6.474a1.875 1.875 0 000-2.651L15.5 4.787a1.875 1.875 0 00-2.651 0l-.1.099V17.25c0 .126-.003.251-.01.375z" />
-                    </svg>
-                    ")" */
 </style>
 
