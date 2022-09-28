ba70e928c0e0ec7c4a7ac5a38eab9f193db3b2db
Marin Nikolli <sibalonat@gmail.com>
2022-09-28 12:51:19 +0200

    .

--- resources/js/Pages/Pinia/PiniaTry.vue
+++ resources/js/Pages/Pinia/PiniaTry.vue
@@ -5,10 +5,12 @@ let counter = useCounterStore()
 console.dir(counter);
 </script>
 <template>
-<div class="w-full">
-    <div class="w-1/2 mx-auto mt-56">
-        <h1>{{ counter.count }}</h1>
-        <button @click="counter.increment()" :disabled="!counter.remaining">Add more ({{ counter.remaining }} remaining)</button>
+    <div class="w-full">
+        <div class="w-1/2 mx-auto mt-10">
+            <h1>{{ counter.count }}</h1>
+            <button @click="counter.increment()" :disabled="!counter.remaining">Add more ({{ counter.remaining }}
+                remaining)</button>
+        </div>
+
     </div>
-</div>
 </template>
