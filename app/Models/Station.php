f67387c4466080cf91cfbfb17a6c74d51e4ddd4c
Marin Nikolli <sibalonat@gmail.com>
2022-09-29 10:49:39 +0200

    initial conf

--- app/Models/Station.php
+++ app/Models/Station.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Station extends Model
 {
     use HasFactory;
+    public static function booted()
+    {
+        static::creating(function(Station $station) {
+            $station->uuid = Str::uuid();
+            if (!$station->slug) {
+                $station->slug = $station->uuid;
+            }
+        });
+    }
+
+    protected $fillable = ['slug', 'uuid', 'teaser', 'tour_id', 'lng', 'lat'];
 }
--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -2,10 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tour extends Model
 {
     use HasFactory;
+
+    public static function booted()
+    {
+        static::creating(function(Tour $tour) {
+            $tour->uuid = Str::uuid();
+            if (!$tour->slug) {
+                $tour->slug = $tour->uuid;
+            }
+        });
+    }
+
+    protected $fillable = ['uuid', 'title', 'slug'];
 }
