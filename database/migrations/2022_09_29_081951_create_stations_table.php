870ef3041e02c094498fa95cdfff224a2aae5a6f
Marin Nikolli <sibalonat@gmail.com>
2022-09-29 10:27:28 +0200

    virtual tours

---
+++ database/migrations/2022_09_29_081951_create_stations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Tour;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stations', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->uuid('uuid')->unique();
+            $table->text('teaser')->nullable();
+            $table->foreignIdFor(Tour::class);
+            $table->string('lng')->default(0);
+            $table->string('lat')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stations');
+    }
+};
